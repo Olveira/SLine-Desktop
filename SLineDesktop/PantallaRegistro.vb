833c251fbab3ffb65b3b3686fcbb16f55979fa72
fetovic <49279612+fetovic@users.noreply.github.com>
2020-07-09 09:20:35 -0300

    subi los cambios de combo box de registro

--- SLineDesktop/PantallaRegistro.vb
+++ SLineDesktop/PantallaRegistro.vb
@@ -2,4 +2,18 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         InicioADM.ShowDialog()
     End Sub
+
+    Private Sub PantallaRegistro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cbxSexo.Items.Add("Masculino")
+        cbxSexo.Items.Add("Femenino")
+        cbxSexo.Items.Add("Otro")
+    End Sub
+
+    Private Sub CerrarRegistro(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+
+        cbxSexo.Items.Clear()
+
+    End Sub
+
+
 End Class
