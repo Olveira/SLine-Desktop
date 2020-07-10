4075f5f86b810d9d14333539a2abd401dae4714e
Agustin Alvez <agustin.alvez@anima.edu.uy>
2020-07-10 16:20:08 -0300

    entidades listo y concexion listo

--- SLineDesktop/Entidades/caso.vb
+++ SLineDesktop/Entidades/caso.vb
@@ -1,3 +1,53 @@
 ﻿Public Class caso
+    Private _nombre As String
+    Private _id As Integer
+    Private _descripcion As String
+    Private _coordenadas As String
 
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+    Public Property Id As Integer
+        Get
+            Return _id
+        End Get
+        Set(value As Integer)
+            _id = value
+        End Set
+    End Property
+
+    Public Property Descripcion As String
+        Get
+            Return _descripcion
+        End Get
+        Set(value As String)
+            _descripcion = value
+        End Set
+    End Property
+
+    Public Property Coordenadas As String
+        Get
+            Return _coordenadas
+        End Get
+        Set(value As String)
+            _coordenadas = value
+        End Set
+    End Property
+
+    Public Sub New(nombre As String, id As Integer, descripcion As String, coordenadas As String)
+        _nombre = nombre
+        _id = id
+        _descripcion = descripcion
+        _coordenadas = coordenadas
+    End Sub
+
+    Public Sub New()
+
+    End Sub
 End Class
--- SLineDesktop/Entidades/usuario.vb
+++ SLineDesktop/Entidades/usuario.vb
@@ -1,3 +1,89 @@
 ﻿Public Class usuario
+    Private _username As String
+    Private _email As String
+    Private _password As String
+    Private _sexo As String
+    Private _fechaNac As Date
+    Private _rol As String
+    Private _token As Integer
+    Private _id As Integer
 
+    Public Property Username As String
+        Get
+            Return _username
+        End Get
+        Set(value As String)
+            _username = value
+        End Set
+    End Property
+    Public Property Email As String
+        Get
+            Return _email
+        End Get
+        Set(value As String)
+            _email = value
+        End Set
+    End Property
+    Public Property Password As String
+        Get
+            Return _password
+        End Get
+        Set(value As String)
+            _password = value
+        End Set
+    End Property
+    Public Property Sexo As String
+        Get
+            Return _sexo
+        End Get
+        Set(value As String)
+            _sexo = value
+        End Set
+    End Property
+    Public Property FechaNac As Date
+        Get
+            Return _fechaNac
+        End Get
+        Set(value As Date)
+            _fechaNac = value
+        End Set
+    End Property
+    Public Property Rol As String
+        Get
+            Return _rol
+        End Get
+        Set(value As String)
+            _rol = value
+        End Set
+    End Property
+    Public Property Token As Integer
+        Get
+            Return _token
+        End Get
+        Set(value As Integer)
+            _token = value
+        End Set
+    End Property
+    Public Property Id As Integer
+        Get
+            Return _id
+        End Get
+        Set(value As Integer)
+            _id = value
+        End Set
+    End Property
+
+    Public Sub New(username As String, email As String, password As String, sexo As String, fechaNac As Date, rol As String, token As Integer, id As Integer)
+        _username = username
+        _email = email
+        _password = password
+        _sexo = sexo
+        _fechaNac = fechaNac
+        _rol = rol
+        _token = token
+        _id = id
+    End Sub
+    Public Sub New()
+
+    End Sub
 End Class
