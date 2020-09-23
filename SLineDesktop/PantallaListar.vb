faa742414b2cf0e3e7496bd0a943fed7ca770d80
Agustin Alvez <agustin.alvez@anima.edu.uy>
2020-09-23 12:13:03 -0300

    subir cambios en pantalla listar

--- SLineDesktop/PantallaListar.vb
+++ SLineDesktop/PantallaListar.vb
@@ -85,4 +85,31 @@
 
         End While
     End Sub
+
+    Private Sub PantallaListar_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        LVListadoUsuarios.Items.Clear()
+        Dim logica As New logicaUsuario
+        Dim lista As New List(Of usuario)
+        lista = logica.listarPersona()
+        Dim i As Integer
+        i = logica.listarPersona.Count - 1
+        Dim item As ListViewItem
+        Dim arra(7) As String
+
+        While i <> -1
+            arra(0) = lista(i).Username
+            arra(1) = lista(i).Email
+            arra(2) = lista(i).Password
+            arra(3) = lista(i).Sexo
+            arra(4) = lista(i).FechaNac
+            arra(5) = lista(i).Rol
+            arra(6) = lista(i).Id.ToString
+
+            item = New ListViewItem(arra)
+            LVListadoUsuarios.Items.Add(item)
+            i = i - 1
+
+
+        End While
+    End Sub
 End Class
