8baee2c66eae2e3de365dc81c3311ddb75304525
F. Olveira <49279424+Olveira@users.noreply.github.com>
2020-09-29 12:26:48 -0300

    Funciones
    Listar casos y declinar casos funcionando

--- SLineDesktop/Logica/logicaCaso.vb
+++ SLineDesktop/Logica/logicaCaso.vb
@@ -2,7 +2,6 @@
     Public Function listarCaso() As List(Of caso)
         Dim persistencia As New persistenciaCaso
         Return persistencia.listarCasos()
-
     End Function
 
     Public Sub eliminarCaso(idCaso As Integer)
